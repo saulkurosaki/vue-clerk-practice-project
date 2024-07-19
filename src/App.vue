9a628a0a25dc1a2e0acc75a67bfac36a9b3db022
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-19 12:37:18 -0600

    feat(App): implement the header and top nav bar

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,20 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <header class="bg-[#6c47ff]">
+    <div class="container flex justify-between items-center h-20 w-full">
+      <div
+        @click="$router.push('/')"
+        class="text-2xl font-extrabold text-white cursor-pointer"
+      >
+        ClerkVueAuth
+      </div>
+      <nav class="flex items-center gap-10 text-white text-xl font-semibold">
+        <RouterLink to="/sign-in">SignIn</RouterLink>
+        <RouterLink to="/sign-up">SignUp</RouterLink>
+      </nav>
+    </div>
+  </header>
+
   <RouterView />
 </template>
