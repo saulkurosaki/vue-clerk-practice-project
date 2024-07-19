03e5da4f81f5e125e9274a8c73c8085671fac3ad
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-19 12:51:08 -0600

    feat(SignUp): implement the SignUp view component initial layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignIn from "@/views/SignIn.vue";
+import SignUp from "@/views/SignUp.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/sign-in",
       name: "signin",
       component: SignIn,
+    },
+    {
+      path: "/sign-up",
+      name: "signup",
+      component: SignUp,
     },
   ],
 });
